cd206944600e1bd9bb344cee09bff01bfb0a1839
tomhughes96 <32062211+tomhughes96@users.noreply.github.com>
2017-10-05 10:06:34 +0100

    Delete Pieces.py

--- Chess/Pieces.py
+++
@@ -1,9 +0,0 @@
-class Piece:
-    def __init__(self, name, diag, horiverti): #does not include pawns or knights
-        self.name = name
-        self.diag = diag           #move diagonally?
-        self.horiverti = horiverti #horizontally or vertically?
-King = Piece('Ki', 'y', 'y')
-Queen = Piece('Qu', 'y', 'y')
-Bishop = Piece('Bi', 'y', 'n')
-Rook = Piece('Ro', 'n', 'y')
